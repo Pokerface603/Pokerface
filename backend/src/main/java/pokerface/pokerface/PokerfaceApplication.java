d77c023dca2230a3861dff240750d17dd2e05723
seo-jio <lio8625@naver.com>
2023-07-20 17:44:35 +0900

    [S09P12A603-69] feat: 백엔드 스프링 부트 프로젝트 초기화

---
+++ backend/src/main/java/pokerface/pokerface/PokerfaceApplication.java
@@ -0,0 +1,13 @@
+package pokerface.pokerface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerfaceApplication.class, args);
+	}
+
+}
