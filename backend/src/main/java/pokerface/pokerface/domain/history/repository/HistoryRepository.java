0e989682b88206214225a7e9dbd73f24b5508a2c
이현구 <wancern@naver.com>
2023-07-31 14:52:52 +0900

    [S09P12A603-252] Style: Spring Data JPA 변환

--- backend/src/main/java/pokerface/pokerface/domain/history/repository/HistoryRepository.java
+++ backend/src/main/java/pokerface/pokerface/domain/history/repository/HistoryRepository.java
@@ -1,11 +1,11 @@
 package pokerface.pokerface.domain.history.repository;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import pokerface.pokerface.domain.history.entity.History;
 
 @Repository
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class HistoryRepository {
+public interface HistoryRepository extends JpaRepository<History, Long> {
 }
