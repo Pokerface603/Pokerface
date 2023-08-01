2a2b9278481983907b73f7c47d7e206d19f4b4e9
임대영 <ldy092000@gmail.com>
2023-08-01 16:29:41 +0900

    [S09P12A603-349] style : 양피지 컴포넌트 코드 포맷 수정

--- frontend/src/components/Parchment.jsx
+++ frontend/src/components/Parchment.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Parchment = ({ width, height, children }) => {
   return (
-    <div className="bg-parchment-box" style={{ width: width, height: height }}>
+    <div className="bg-parchment-box" style={{ width, height }}>
       {children}
     </div>
   );
