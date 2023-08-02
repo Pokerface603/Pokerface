dba8d43e3e251729202b2886140d2b93df5c310f
임대영 <ldy092000@gmail.com>
2023-08-02 13:28:15 +0900

    [S09P12A603-353] feat : 나무 버튼 공통 컴포넌트 구현

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         "parchment-box": "url('/src/assets/images/parchment.svg')",
+        "wood-button": "url('/src/assets/images/WoodButton.svg')",
       },
     },
   },
