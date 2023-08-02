2d1792c04e91e58d0aabcae2d175d4ef45ee7ae3
Hanna Kim <gksskrn0158@gmail.com>
2023-08-02 14:56:40 +0900

    [S09P12A603-355] feat: input 공통 컴포넌트 작성 진행중

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,21 @@
+const Input = ({ width, height, type, placeholder }) => {
+  return (
+    <>
+      <input
+        type={`${type}`}
+        id="input-box"
+        aria-describedby="input box"
+        className="bg-none placeholder-black/[0.5] border-solid border-black/[0.6] border-b-2 text-black focus:outline-none focus:bg-gray-50 block p-2.5"
+        style={{
+          width,
+          height,
+          fontFamily: "Unchained-Spaghetti",
+          fontSize: "45px",
+        }}
+        placeholder={`${placeholder}`}
+      />
+    </>
+  );
+};
+
+export default Input;
