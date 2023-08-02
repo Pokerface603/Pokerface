f197715fe89cf099c621a188447a2545b599f394
Hanna Kim <gksskrn0158@gmail.com>
2023-08-02 15:40:18 +0900

    [S09P12A603-355] chore: margin 추가 및 React.Fragment 제거

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -1,11 +1,10 @@
 const Input = ({ width, height, type, placeholder }) => {
   return (
-    <>
       <input
         type={`${type}`}
         id="input-box"
         aria-describedby="input box"
-        className="bg-none placeholder-black/[0.5] border-solid border-black/[0.6] border-b-2 text-black focus:outline-none focus:bg-gray-50 block p-2.5"
+        className="bg-none placeholder-black/[0.5] border-solid border-black/[0.6] border-b-2 text-black focus:outline-none focus:bg-gray-50 block p-2.5 m-2"
         style={{
           width,
           height,
@@ -14,7 +13,6 @@ const Input = ({ width, height, type, placeholder }) => {
         }}
         placeholder={`${placeholder}`}
       />
-    </>
   );
 };
 
