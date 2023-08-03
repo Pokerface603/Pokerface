9f6feca50615d90becbde08f936f1b0e47c1511c
Hanna Kim <gksskrn0158@gmail.com>
2023-08-03 10:13:30 +0900

    [S09P12A603-356] feat: TierBox 공통 컴포넌트 작성

--- frontend/src/components/TierBox.jsx
+++ frontend/src/components/TierBox.jsx
@@ -1,7 +1,14 @@
-const TierBox = ({tier, width, height}) => {
-    return(
-        <img src={tier} alt={tier} width={width} height={height} />
-    )
-}
+import TierBoxSvg from "../utils/TierBoxSvg";
+
+const TierBox = ({ tier, width, height }) => {
+  return (
+    <img
+      src={TierBoxSvg[tier]}
+      alt={tier}
+      width={width}
+      height={height}
+    />
+  );
+};
 
 export default TierBox;
