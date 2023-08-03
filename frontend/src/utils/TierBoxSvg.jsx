3020f9ca9400ff8c97e7621122330c7ea86c1956
Hanna Kim <gksskrn0158@gmail.com>
2023-08-03 10:08:07 +0900

    [S09P12A603-356] feat: 공통 TierBoxSvg 등록

---
+++ frontend/src/utils/TierBoxSvg.jsx
@@ -0,0 +1,9 @@
+const TierBoxSvg = {
+  A: require("../assets/images/tier/A.svg").default,
+  J: require("../assets/images/tier/J.svg").default,
+  Joker: require("../assets/images/tier/Joker.svg").default,
+  K: require("../assets/images/tier/K.svg").default,
+  Q: require("../assets/images/tier/Q.svg").default,
+};
+
+export default TierBoxSvg;
