6e7f3d006495768ea2800ea128d67f202ae5210b
Hanna Kim <gksskrn0158@gmail.com>
2023-08-03 14:02:43 +0900

    [S09P12A603-356] style: 기존 TierBoxSvg 파일 제거

--- frontend/src/utils/TierBoxSvg.jsx
+++
@@ -1,9 +0,0 @@
-const TierBoxSvg = {
-  A: require("../assets/images/tier/A.svg").default,
-  J: require("../assets/images/tier/J.svg").default,
-  Joker: require("../assets/images/tier/Joker.svg").default,
-  K: require("../assets/images/tier/K.svg").default,
-  Q: require("../assets/images/tier/Q.svg").default,
-};
-
-export default TierBoxSvg;
