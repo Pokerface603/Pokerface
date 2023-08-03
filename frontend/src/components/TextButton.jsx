ea409c4e5730a3f46211e5b4ac33d530c5e927d5
임대영 <ldy092000@gmail.com>
2023-08-03 17:45:15 +0900

    [S09P12A063-319] style : 글자 버튼 코드 간소화

--- frontend/src/components/TextButton.jsx
+++ frontend/src/components/TextButton.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import kakaoLogo from "../assets/images/kakao-logo.svg";
 
 const TextButton = ({ width, height, fontSize, text, onClick }) => {
-  const font = text === "kakao" ? "Unchained-Spaghetti" : "NexonGothic";
+  const isKakao = text === "kakao";
+
+  const font = isKakao ? "Unchained-Spaghetti" : "NexonGothic";
 
   return (
     <div
       className="flex text-center justify-center items-center cursor-pointer hover:shadow-2xl hover:underline hover:opacity-75"
       style={{ width, height }}
       onClick={onClick}>
-      {text === "kakao" && <img src={kakaoLogo} alt="l" />}
+      {isKakao && <img src={kakaoLogo} alt="l" />}
       <span className=" align-bottom" style={{ fontFamily: font, fontSize }}>
         {text}
       </span>
