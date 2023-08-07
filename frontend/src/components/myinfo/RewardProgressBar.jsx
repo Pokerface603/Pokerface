ea7deea9e8bc0fc613a799a9226a3c0180bdf8e6
Hanna Kim <gksskrn0158@gmail.com>
2023-08-07 10:13:38 +0900

    [S09P12A603-313] feat: progressbar 추가

---
+++ frontend/src/components/myinfo/RewardProgressBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const RewardProgressBar = ({
+  reward,
+  totalMatches,
+  background,
+  height,
+  marginTop,
+  style,
+}) => {
+  // totalMatches는 임의로 넣어논 값. 
+  const percentage = (reward / totalMatches) * 100;
+  return (
+    <div
+      className="w-full bg-white"
+      style={{ height, ...style }}
+    >
+      <div
+        style={{
+          width: `${percentage}%`,
+          height,
+          marginTop,
+          background,
+        }}
+      ></div>
+    </div>
+  );
+};
+
+export default RewardProgressBar;
