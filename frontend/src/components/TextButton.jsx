89b45e6ec77dcdd2642ff87c8c51cf2c722edcbd
이국신 <nw4611@naver.com>
2023-08-07 16:50:42 +0900

    [S09P12A603-473] chore: 사용하지 않는 import 제거

--- frontend/src/components/TextButton.jsx
+++ frontend/src/components/TextButton.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import kakaoLogo from "../assets/images/kakao-logo.svg";
 
 const TextButton = ({ width, height, fontSize, text, imgSrc, onClick }) => {
   const isenglish = /^[A-Za-z]+$/;
@@ -12,7 +11,8 @@ const TextButton = ({ width, height, fontSize, text, imgSrc, onClick }) => {
     <div
       className="flex text-center justify-center items-center cursor-pointer hover:shadow-2xl hover:underline hover:opacity-75"
       style={{ width, height }}
-      onClick={onClick}>
+      onClick={onClick}
+    >
       {imgSrc && <img src={imgSrc} alt="logo" />}
       <span className=" align-bottom" style={{ fontFamily, fontSize }}>
         {text}
