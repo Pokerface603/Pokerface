51949e1d6e14bd2f5e6938d6575a01372549f866
Hanna Kim <gksskrn0158@gmail.com>
2023-08-07 10:17:25 +0900

    [S09P12A603-313] feat: myinfo 전체 레이아웃 추가

---
+++ frontend/src/components/myinfo/MyInfo.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Sheriff from "../../assets/images/myinfo/sheriff.svg";
+import LineForMyInfo from "../../assets/images/myinfo/line-for-my-info.svg";
+import TierBox from "@component/TierBox";
+import Parchment from "@component/Parchment";
+import MyInfoHeader from "./MyInfoHeader";
+import MyInfoReward from "./MyInfoReward";
+import MyInfoOdds from "./MyInfoOdds";
+
+const MyInfo = ({
+  width,
+  height,
+  tier,
+  nickname,
+  reward,
+  totalMatches,
+  wins,
+  button,
+}) => {
+  return (
+    <Parchment
+      style={{
+        width,
+        height,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div className="flex mx-2 gap-3 items-center">
+        <TierBox tier={tier} width="135px" height="135px" />
+        <div className="mx-1">
+          <MyInfoHeader nickname={nickname} button={button} />
+          <img src={LineForMyInfo} alt="line for myInfo" />
+          <div className="my-0">
+            <MyInfoReward reward={reward} totalReward={totalMatches} />
+            <MyInfoOdds wins={wins} totalReward={totalMatches} />
+          </div>
+          <img src={LineForMyInfo} alt="line for myInfo" />
+        </div>
+        <img
+          src={Sheriff}
+          style={{
+            top: "110px",
+            right: "110px",
+            position: "absolute",
+            zIndex: 1,
+          }}
+          alt="sheriff"
+        />
+      </div>
+    </Parchment>
+  );
+};
+
+export default MyInfo;
