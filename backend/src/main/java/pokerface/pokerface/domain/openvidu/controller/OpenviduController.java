84b10c249d4f2fe6f78bfed552b463cf4d4227f6
seo-jio <lio8625@naver.com>
2023-08-08 16:40:29 +0900

    [S09P12A603-496] Feat : 방 생성 및 참가에 따른 방 정보 수정 로직 추가

--- backend/src/main/java/pokerface/pokerface/domain/openvidu/controller/OpenviduController.java
+++ backend/src/main/java/pokerface/pokerface/domain/openvidu/controller/OpenviduController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import io.openvidu.java.client.Connection;
@@ -19,6 +20,9 @@ import io.openvidu.java.client.OpenViduJavaClientException;
 import io.openvidu.java.client.Session;
 import io.openvidu.java.client.SessionProperties;
 
+import pokerface.pokerface.config.error.RestException;
+import pokerface.pokerface.config.error.errorcode.ErrorCode;
+import pokerface.pokerface.config.login.PrincipalDetails;
 import pokerface.pokerface.domain.history.entity.GameMode;
 import pokerface.pokerface.domain.room.dto.request.RoomCreateReq;
 import pokerface.pokerface.domain.room.service.RoomService;
@@ -45,27 +49,30 @@ public class OpenviduController {
     private final RoomService roomService;
 
     @PostMapping("/sessions")
-    public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params)
+    public ResponseEntity<String> initializeSession(@RequestBody(required = false) Map<String, Object> params,
+                                                    @AuthenticationPrincipal PrincipalDetails principalDetails)
             throws OpenViduJavaClientException, OpenViduHttpException {
+
+        log.debug(principalDetails.getMember().toString());
 
         SessionProperties properties = SessionProperties.fromJson(params).build();
         Session session = openvidu.createSession(properties);
 
-        roomService.createRoom(session.getSessionId(),
+        roomService.createRoom(session.getSessionId(), principalDetails.getMember(), // 방 생성
                 RoomCreateReq.builder()
                         .title((String) params.get("title"))
-                        .gameMode(GameMode.valueOf((String) params.get("mode")))
+                        .gameMode(GameMode.valueOf((String) params.get("gameMode")))
                         .isPrivate((boolean) params.get("isPrivate"))
                         .roomPassword((String) params.get("roomPassword"))
                         .build());
 
-        //sessionId : session 생성 시 입력한 세션 이름
         return new ResponseEntity<>(session.getSessionId(), HttpStatus.OK);
     }
 
 
     @GetMapping("/sessions")
     public ResponseEntity<?> getActiveSessions(){
+        System.out.println("openvidu.getActiveSessions().size() = " + openvidu.getActiveSessions().size());
         openvidu.getActiveSessions().
                 forEach(session -> System.out.println("session.getSessionId() = " + session.getSessionId()));
 
@@ -74,31 +81,31 @@ public class OpenviduController {
 
     @PostMapping("/sessions/{sessionId}/connections")
     public ResponseEntity<String> createConnection(@PathVariable("sessionId") String sessionId,
-                                                   @RequestBody(required = false) Map<String, Object> params)
+                                                   @RequestBody(required = false) Map<String, Object> params,
+                                                   @AuthenticationPrincipal PrincipalDetails principalDetails)
             throws OpenViduJavaClientException, OpenViduHttpException {
 
-        Session session = openvidu.getActiveSession(sessionId);
+        String inputPassword = (String) params.get("password");
+        log.debug("비밀번호 검증 : {}", inputPassword);
+        if(!inputPassword.equals("")) { // 비빌번호가 있는 방에 참가하려는 경우
+            if(!roomService.findRoomById(sessionId).getRoomPassword().equals(inputPassword)) { // 비밀번호 불일치
+                throw new RestException(ErrorCode.INVALID_PASSWORD);
+            }
+        }
 
-        if (session == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        Session session = openvidu.getActiveSession(sessionId);
+        log.debug("방 인원 검증 : {}", session.getConnections().size());
+        if (session.getConnections().size() >= 2) { // 방에 두 명 넘게 접속을 시도할 경우
+            throw new RestException(ErrorCode.ALREADY_FULL);
         }
 
         ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
         Connection connection = session.createConnection(properties);
 
+        log.debug("새로운 인원 참가에 인한 방 정보 갱신");
+        roomService.joinRoom(sessionId, principalDetails.getMember()); // 방 정보 갱신
+
         return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
     }
-
-    @PostMapping("/sessions/{sessionId}/{email}/disconnections")
-    public ResponseEntity<Void> disConnection(@PathVariable("sessionId") String sessionId,
-                                              @PathVariable String email,
-                                              @RequestBody(required = false) Map<String, Object> params)
-            throws OpenViduJavaClientException, OpenViduHttpException {
-
-        roomService.removeMember(sessionId, email);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
 }
 
