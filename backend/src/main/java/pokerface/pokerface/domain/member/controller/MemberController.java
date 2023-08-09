eff6ae4dee241cb8b7ed4e4c062544d9cf6323ba
이현구 <wancern@naver.com>
2023-08-09 11:28:03 +0900

    [S09P12A603-440] Feat: TOP 5 랭킹 기능 구현

--- backend/src/main/java/pokerface/pokerface/domain/member/controller/MemberController.java
+++ backend/src/main/java/pokerface/pokerface/domain/member/controller/MemberController.java
@@ -9,8 +9,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pokerface.pokerface.config.login.PrincipalDetails;
 import pokerface.pokerface.domain.member.dto.request.MemberJoinReq;
+import pokerface.pokerface.domain.member.dto.response.RankingRes;
 import pokerface.pokerface.domain.member.entity.Member;
 import pokerface.pokerface.domain.member.service.MemberService;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -54,5 +57,23 @@ public class MemberController {
     @GetMapping("/check/nickname/{nickname}")
     public ResponseEntity<Boolean> nicknameDuplicatedCheck(@PathVariable String nickname) {
         return new ResponseEntity<>(memberService.nicknameCheck(nickname), HttpStatus.OK);
+    }
+
+    /*
+     * 랭킹 관련 API
+     * */
+    @GetMapping("/ranking")
+    public ResponseEntity<List<RankingRes>> findAllRankings(){
+        return new ResponseEntity<>(memberService.findAllRankings(), HttpStatus.OK);
+    }
+
+    @GetMapping("/ranking/top")
+    public ResponseEntity<List<RankingRes>> findTop5Rankings(){
+        return new ResponseEntity<>(memberService.findTop5Rankings(), HttpStatus.OK);
+    }
+
+    @GetMapping("/ranking/{memberId}")
+    public ResponseEntity<RankingRes> findRankingByMemberId(@PathVariable Long memberId){
+        return new ResponseEntity<>(memberService.findRankingByMemberId(memberId), HttpStatus.OK);
     }
 }
