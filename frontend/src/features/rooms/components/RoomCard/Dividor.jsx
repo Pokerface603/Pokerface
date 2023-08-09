66dbe49aa06ad518a442d9b41bf70ccaf77033be
이국신 <nw4611@naver.com>
2023-08-09 09:27:16 +0900

    [S09P12A603-474] feat: RoomCard 구분선 구현

---
+++ frontend/src/features/rooms/components/RoomCard/Dividor.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Dividor() {
+  return (
+    <div
+      style={{
+        boxSizing: "border-box",
+        border: "0.5px solid #cdcdcd",
+        height: "1px",
+      }}
+    ></div>
+  );
+}
+
+export default Dividor;
