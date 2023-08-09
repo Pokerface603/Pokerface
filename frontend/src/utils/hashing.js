49547d1e12f87fe63f279459098a3853936826a6
이국신 <nw4611@naver.com>
2023-08-10 00:36:43 +0900

    [S09P12A603-490] feat : 방에 참가할 수 없는 에러 해결

--- frontend/src/utils/hashing.js
+++ frontend/src/utils/hashing.js
@@ -1,11 +1,6 @@
 import CrpytoJS from "crypto-js";
 
 export function hashOpenviduTitle(title) {
-  const encrypted = CrpytoJS.AES.encrypt(title, "secretKey")
-    .toString()
-    .replace(/\+/g, "p1L2u3S")
-    .replace(/\//g, "s1L2a3S4h")
-    .replace(/=/g, "e1Q2u3A4l");
-
+  const encrypted = CrpytoJS.SHA256(title, "secretKey").toString();
   return encrypted;
 }
