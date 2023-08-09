2a2b9cfa8675e0dd4791f8aca6b431c47cbcbeb0
이국신 <nw4611@naver.com>
2023-08-09 16:13:19 +0900

    [S09P12A603-479] feat: 페이지 네비게이터 컴포넌트 구현

---
+++ frontend/src/features/rooms/components/Paging/Navigator.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import NavigatorPageButton from "./NavigatorPageButton";
+
+function Navigator({ totalPage, onClickPage }) {
+  return (
+    <nav aria-label="Page navigation example" className="bg-white">
+      <ul class="list-style-none flex">
+        {Array.from({ length: totalPage }, (v, i) => i + 1).map((pageNum) => (
+          <NavigatorPageButton
+            id={pageNum}
+            pageNum={pageNum}
+            onClick={onClickPage}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigator;
