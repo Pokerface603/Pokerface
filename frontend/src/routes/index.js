bcebd467e98ca621b0b128745b359bd6cb54f6b8
이국신 <nw4611@naver.com>
2023-08-09 17:39:16 +0900

    [S09P12A603-490] feat : 로그인 페이지 라우팅 적용

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,8 +1,10 @@
 import GamePage from "@feature/Game/routes/GamePage";
+import LoginFormPage from "@feature/login/components/LoginFormPage";
 import RoomsPage from "@feature/rooms/components/RoomsPage";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
+  { path: "/", element: <LoginFormPage /> },
   { path: "/lobby", element: <RoomsPage /> },
   { path: "/game/:sessionId", element: <GamePage /> },
 ]);
