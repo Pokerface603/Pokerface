ef507bfc53b3b95df1307da5aa20f35ddb66e4b7
이국신 <nw4611@naver.com>
2023-08-09 11:08:17 +0900

    [S09P12A603-473] feat: 검색어를 통한 방 검색 API 연동

--- frontend/src/features/rooms/api/room.js
+++ frontend/src/features/rooms/api/room.js
@@ -9,3 +9,8 @@ export const getRooms = async (mode) => {
   const { data } = await axios.get(`/rooms/mode/${mode}`);
   return data;
 };
+
+export const searchRoomsWithKeyword = async (mode, title) => {
+  const { data } = await axios.get(`/rooms/mode/${mode}/title/${title}`);
+  return data;
+};
