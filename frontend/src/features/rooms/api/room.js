2048d0eab23af471e2daf8c89fa3380fdcb2f5a4
이국신 <nw4611@naver.com>
2023-08-09 14:29:27 +0900

    [S09P12A603-476] refactor: 불필요한 메소드 제거

--- frontend/src/features/rooms/api/room.js
+++ frontend/src/features/rooms/api/room.js
@@ -1,9 +1,4 @@
-import { getToken } from "./session";
 import { axios } from "@lib/axios";
-
-export const makeRoom = async (roomData) => {
-  return await getToken(roomData);
-};
 
 export const getRooms = async (mode) => {
   const { data } = await axios.get(`/rooms/mode/${mode}`);
