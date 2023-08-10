79677d01964a34097ea6c43f011566e7711fb645
Hanna Kim <gksskrn0158@gmail.com>
2023-08-10 16:16:18 +0900

    [S09P12A603-464] feat: caps lock 감지 위한 onkeydown 속성 추가

--- frontend/src/components/Input.jsx
+++ frontend/src/components/Input.jsx
@@ -7,6 +7,7 @@ const Input = ({
   width,
   height,
   style,
+  onKeyDown,
 }) => {
   return (
     <input
@@ -15,9 +16,10 @@ const Input = ({
       value={value}
       placeholder={placeholder}
       id={id}
+      onKeyDown={onKeyDown}
       aria-describedby="input box"
       required
-      className="bg-transparent block placeholder-black/[0.5] border-solid border-black/[0.6] border-b-2 text-black focus:outline-none focus:bg-gray-50/20 p-2.5 m-2"
+      className="bg-transparent block placeholder-black/[0.5] border-solid border-black/[0.6] border-b-2 text-black focus:outline-none focus:bg-gray-50/20 p-2.5 mt-2 mr-2 ml-2"
       style={{
         width,
         height,
