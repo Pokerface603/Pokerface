e12c5654819d63fd9a4a2631350ea505657e3fb4
seo-jio <lio8625@naver.com>
2023-08-11 13:11:03 +0900

    [S09P12A603-430] Feat : 소셜 로그인 성공, 실패를 처리할 Handler 추가

---
+++ backend/src/main/java/pokerface/pokerface/config/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,51 @@
+package pokerface.pokerface.config.oauth.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+import pokerface.pokerface.config.jwt.service.JwtService;
+import pokerface.pokerface.config.oauth.CustomOAuth2User;
+import pokerface.pokerface.domain.member.service.MemberService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler{
+	
+	private final JwtService jwtService;
+	private final MemberService memberService;
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		log.debug("소셜 로그인 성공");
+		
+		CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+		
+		String accessToken = jwtService.createAccessToken(oAuth2User.getEmail(), oAuth2User.getNickname());
+		String refreshToken = jwtService.createRefreshToken();
+		
+		memberService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
+
+		// 1. local test 버전
+//		String uri = UriComponentsBuilder.fromUriString("http://localhost:3000/lobby")
+//                .queryParam("accessToken", accessToken)
+//                .queryParam("refreshToken", refreshToken)
+//                .build().toUriString();
+		// 2. 배포 버전
+		String uri = UriComponentsBuilder.fromUriString("https://pokerface-server.ddns.net/lobby")
+				.queryParam("accessToken", accessToken)
+				.queryParam("refreshToken", refreshToken)
+				.build().toUriString();
+        response.sendRedirect(uri);
+	}
+	
+}
