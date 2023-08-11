9505245db97813b48736d33af08eb38e8b1c69e0
이현구 <wancern@naver.com>
2023-08-11 13:34:21 +0900

    [S09P12A603-441] Feat: Match 도메인 삭제 및 기능 이동

--- backend/src/main/java/pokerface/pokerface/domain/lobby/service/LobbyService.java
+++ backend/src/main/java/pokerface/pokerface/domain/lobby/service/LobbyService.java
@@ -2,10 +2,19 @@ package pokerface.pokerface.domain.lobby.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pokerface.pokerface.config.error.RestException;
+import pokerface.pokerface.config.error.errorcode.ErrorCode;
 import pokerface.pokerface.domain.friend.service.FriendService;
 import pokerface.pokerface.domain.member.service.MemberService;
+import pokerface.pokerface.domain.room.dto.response.RoomInfoRes;
+import pokerface.pokerface.domain.room.entity.Room;
+import pokerface.pokerface.domain.room.repository.RoomRepository;
+import pokerface.pokerface.domain.room.service.RoomService;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @Transactional
@@ -13,9 +22,10 @@ import javax.transaction.Transactional;
 public class LobbyService {
 
     private final MemberService memberService;
-
+    private final RoomService roomService;
     private final FriendService friendService;
 
+    private final RoomRepository roomRepository;
 
     public String findNickNameByEmail(String email){
         return memberService.findByEmail(email).getNickname();
@@ -23,5 +33,49 @@ public class LobbyService {
 
     public void saveFriends(String firstEmail, String secondEmail){
         friendService.save(firstEmail, secondEmail);
+    }
+
+    public RoomInfoRes findRoomByRating(String email) {
+        Integer rating = memberService.findByEmail(email).getRating();
+
+        for(int times = 1; times <= 6; times++){
+            List<Room> rooms = findRoomsByMaxRatingAndMinRating(rating + 50 * times, Math.max(0, rating - 50 * times));
+
+            try {
+                if(rooms.isEmpty()){
+                    Thread.sleep(3000);
+                    continue;
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RestException(ErrorCode.MATCHING_FAILED);
+            }
+            return roomService.findRoomInfoById(findProximateRoom(rooms, rating));
+        }
+        throw new RestException(ErrorCode.MATCHING_FAILED);
+    }
+
+    public String findProximateRoom(List<Room> rooms, Integer rating){
+        int gap = Integer.MAX_VALUE;
+        Room target = null;
+
+        for(Room room : rooms){
+            int now = Math.abs(room.getMembers().get(0).getRating() - rating);
+            if(gap > now){
+                gap = now;
+                target = room;
+            }
+        }
+        assert target != null;
+        return target.getSessionId();
+    }
+    public List<Room> findRoomsByMaxRatingAndMinRating(Integer maxRating, Integer minRating){
+        Iterable<Room> rooms = roomRepository.findAll();
+
+        return StreamSupport.stream(rooms.spliterator(), false)
+                .filter(room -> room.getMembers().size() == 1)
+                .filter(room -> room.getMembers().get(0).getRating() < maxRating)
+                .filter(room -> room.getMembers().get(0).getRating() > minRating)
+                .collect(Collectors.toList());
     }
 }
