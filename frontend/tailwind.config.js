f5154f2c13e9ae785602212972538874da11d8b2
Manna-na <gksskrn0158@gmail.com>
2023-08-14 16:48:50 +0900

    [S09P12A603-584] feat: 로딩 페이지 구현

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -27,6 +27,21 @@ module.exports = {
         ace: "#482613",
         joker: "ffd217",
       },
+      animation: {
+        text: "text 5s ease infinite",
+      },
+      keyframes: {
+        text: {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
     },
   },
   plugins: [],
