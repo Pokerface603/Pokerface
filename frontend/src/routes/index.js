2f11fe16b512d9b55c8f6e4fa084e606882c4cb5
Manna-na <gksskrn0158@gmail.com>
2023-08-14 16:59:29 +0900

    [S09P12A603-585] feat: 로고 페이지 구현

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,12 +2,14 @@ import GamePage from "@feature/Game/routes/GamePage";
 import LoginFormPage from "@feature/login/components/LoginFormPage";
 import RoomsPage from "@feature/rooms/components/RoomsPage";
 import RegistPage from "@feature/regist/component/RegistPage";
+import LogoPage from "../components/LogoPage";
 import { createBrowserRouter } from "react-router-dom";
 import MyPage from "@feature/mypage/component/MyPage";
 import Redirect from "@feature/login/routes/Redirect";
 
 const router = createBrowserRouter([
-  { path: "/", element: <LoginFormPage /> },
+  { path: "/", element: <LogoPage /> },
+  { path: "/login", element: <LoginFormPage /> },
   { path: "/lobby", element: <RoomsPage /> },
   { path: "/game/:sessionId", element: <GamePage /> },
   { path: "/regist", element: <RegistPage /> },
