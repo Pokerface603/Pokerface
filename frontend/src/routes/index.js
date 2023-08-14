60ad8e85ead2da486e5b3aa65e99a4545ee59d5e
임대영 <ldy092000@gmail.com>
2023-08-14 10:24:39 +0900

    [S09P12A063-393] feat : myPage 라우팅 및 hover 이벤트 추가

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,12 +3,14 @@ import LoginFormPage from "@feature/login/components/LoginFormPage";
 import RoomsPage from "@feature/rooms/components/RoomsPage";
 import RegistPage from "@feature/regist/component/RegistPage";
 import { createBrowserRouter } from "react-router-dom";
+import MyPage from "@feature/mypage/component/MyPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <LoginFormPage /> },
   { path: "/lobby", element: <RoomsPage /> },
   { path: "/game/:sessionId", element: <GamePage /> },
   { path: "/regist", element: <RegistPage /> },
+  { path: "/mypage", element: <MyPage /> },
 ]);
 
 export default router;
