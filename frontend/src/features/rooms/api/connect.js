618e2aab2958bd4dced814ca00b1c0259d8955f6
이국신 <nw4611@naver.com>
2023-08-16 02:10:12 +0900

    [S09P12A603-549] feat: 방어 코드 추가

--- frontend/src/features/rooms/api/connect.js
+++ frontend/src/features/rooms/api/connect.js
@@ -2,10 +2,10 @@ import { axios } from "@lib/axios";
 
 export const getLobbies = async (email) => {
   const { data } = await axios.get(`/lobbies/${email}`);
-  return data;
+  return data ?? [];
 };
 
 export const getFriends = async (email) => {
   const { data } = await axios.get(`/lobbies/friend/${email}`);
-  return data;
+  return data ?? [];
 };
