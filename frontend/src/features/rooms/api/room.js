db15947659d130874decbf9b677e66c034136f75
이국신 <nw4611@naver.com>
2023-08-17 22:13:25 +0900

    [S09P12A603-628] fix: 방 나가기 에러 처리

--- frontend/src/features/rooms/api/room.js
+++ frontend/src/features/rooms/api/room.js
@@ -23,8 +23,12 @@ export const quickStart = async (email) => {
 };
 
 export const leaveRoom = async ({ email, sessionId }) => {
-  await axios.post(`/rooms/disconnect`, {
-    email,
-    sessionId,
-  });
+  await axios
+    .post(`/rooms/disconnect`, {
+      email,
+      sessionId,
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
